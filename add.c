12c831b9b455c952c1e8c0472da0d8f6f8b93ab8
SHABNA-k <145551438+SHABNA-k@users.noreply.github.com>
2023-12-09 09:34:54 +0530

    add

---
+++ add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter two number:");
+    scanf("%d %d",&a,&b);
+    sum=a+b;
+    printf("sum of two number=%d",sum);
+    return 0;
+}
